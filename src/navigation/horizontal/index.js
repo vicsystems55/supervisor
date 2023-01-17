a783e81afb53dfbe4cc0cdc67ce64f452b76e3ab
vicSystems <victorasuquob@gmail.com>
2023-01-17 10:15:05 +0100

    added menus

--- src/navigation/horizontal/index.js
+++ src/navigation/horizontal/index.js
@@ -6,6 +6,56 @@ export default [
     route: '/',
   },
   {
+    header: 'Programme',
+    icon: 'FileIcon',
+    title: 'Programme',
+    route: '/',
+  },
+  {
+    header: 'Contracts',
+    icon: 'FileIcon',
+    title: 'Contracts',
+    route: '/',
+  },
+
+  {
+    header: 'Lots',
+    icon: 'FileIcon',
+    title: 'Lots',
+    route: '/',
+  },
+
+  {
+    header: 'Sites',
+    icon: 'FileIcon',
+    title: 'Sites',
+    route: '/',
+  },
+
+  {
+    header: 'Reports',
+    icon: 'FileIcon',
+    title: 'Reports',
+    children: [
+      {
+        title: 'Contract Reports',
+        route: 'home',
+        icon: 'HomeIcon',
+      },
+      {
+        title: 'Lot Report',
+        route: 'second-page',
+        icon: 'FileIcon',
+      },
+    ],
+  },
+  {
+    header: 'Valuation',
+    icon: 'FileIcon',
+    title: 'Valuation',
+    route: '/',
+  },
+  {
     header: 'Pages',
     icon: 'FileIcon',
     children: [
