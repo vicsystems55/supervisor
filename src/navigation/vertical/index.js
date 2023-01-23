3d9a4c7009a4b0a876c70c3dcb5155c849588633
vicSystems <victorasuquob@gmail.com>
2023-01-23 09:55:03 +0100

    added pages

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -1,12 +1,72 @@
 export default [
   {
+    header: 'Home',
+    icon: 'FileIcon',
     title: 'Home',
     route: 'home',
-    icon: 'HomeIcon',
   },
   {
-    title: 'Second Page',
-    route: 'second-page',
+    header: 'Programme',
     icon: 'FileIcon',
+    title: 'Programme',
+    route: 'programmes',
+  },
+  {
+    header: 'Contracts',
+    icon: 'FileIcon',
+    title: 'Contracts',
+    route: 'contracts',
+  },
+  {
+    header: 'Sites',
+    icon: 'FileIcon',
+    title: 'Sites',
+    route: 'sites',
+  },
+
+  {
+    header: 'Reports',
+    icon: 'FileIcon',
+    title: 'Reports',
+    children: [
+      {
+        title: 'Contract Reports',
+        route: 'home',
+        icon: 'HomeIcon',
+      },
+      {
+        title: 'Lot Reports',
+        route: 'second-page',
+        icon: 'FileIcon',
+      },
+    ],
+  },
+  {
+    header: 'Valuation',
+    icon: 'FileIcon',
+    title: 'Valuation',
+    route: '/',
+  },
+  {
+    header: 'User Management',
+    icon: 'FileIcon',
+    title: 'User Management',
+    route: 'users',
+  },
+  {
+    header: 'Pages',
+    icon: 'FileIcon',
+    children: [
+      {
+        title: 'Home',
+        route: 'home',
+        icon: 'HomeIcon',
+      },
+      {
+        title: 'Second Page',
+        route: 'second-page',
+        icon: 'FileIcon',
+      },
+    ],
   },
 ]
